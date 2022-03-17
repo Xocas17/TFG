e125aaaff423dbd12ce8ffa1ba7d7de9919e979b
Xocas17 <xocas89@gmail.com>
2022-03-17 15:10:20 +0000

    avances

---
+++ public/js/index.js
@@ -0,0 +1,84 @@
+function cambiarcontraseña() {
+  $("html").css("cursor", "progress");
+var correo = document.getElementById("email").value.toLowerCase();
+if (correo.length <=1) {
+  $("html").css("cursor", "default");
+  alert("Antes debes escribir tu dirección de correo en el campo de login de usuario para que podamos enviarte el email de recuperación");
+  return;
+} else {
+  firebase.auth().sendPasswordResetEmail(correo);
+  $("html").css("cursor", "default");
+  alert("Has recibido un correo con las instrucciones para cambiar tu contraseña");
+
+}
+
+}
+function toggleSignIn() {
+$("html").css("cursor", "progress");
+if (firebase.auth().currentUser) {
+var current_user = firebase.auth().currentUser;
+var email = document.getElementById("email").value;
+email = email.toLowerCase();
+var tipocuenta;
+if (email.localeCompare(current_user.email) == 0) {
+
+
+} else {
+
+alert(
+"Había una sesión iniciada de otro usuario, cerrando sesión, vuelva a loguearse"
+);
+
+firebase.auth().signOut();
+}
+$("html").css("cursor", "default");
+} else {
+var email = document.getElementById("email").value;
+email = email.toLowerCase();
+var password = document.getElementById("password").value;
+if (email.length < 4) {
+$("html").css("cursor", "default");
+alert("Introduce un correo válido.");
+return;
+}
+if (password.length < 4) {
+$("html").css("cursor", "default");
+alert("La contraseña es demasiado corta.");
+return;
+}
+// Sign in with email and pass.
+// [START authwithemail]
+
+firebase
+.auth()
+.signInWithEmailAndPassword(email, password)
+.then(function (user) {
+// user signed in
+
+var db = firebase.firestore();
+var docRef = db.collection("usuarios").doc(email);
+docRef.get().then(function (doc) {
+  if (doc && doc.exists) {
+    $("html").css("cursor", "default");
+    //}
+  }
+});
+})
+.catch(function (error) {
+var errorCode = error.code;
+var errorMessage = error.message;
+
+if (errorCode === "auth/wrong-password") {
+  alert("Contraseña errónea.");
+} else {
+  alert(errorMessage);
+}
+console.log(error);
+document.getElementById("quickstart-sign-in").disabled = false;
+$("html").css("cursor", "default");
+});
+
+// [END authwithemail]
+}
+
+}
---
+++ public/js/registro.js
@@ -0,0 +1,168 @@
+$(document).ready(function(){
+  var select = document.getElementById("pais")
+  M.FormSelect.init(select);
+  var select = document.getElementById("provincia")
+  M.FormSelect.init(select);  
+  var select = document.getElementById("perfil")
+  M.FormSelect.init(select);
+  console.log("db",db)
+});
+function comprobarPais(){
+  var pais= document.getElementById("pais").value;
+  if(pais=="Spain"){
+    document.getElementById("divProvincia").style.display="block";
+  }
+  else{
+    document.getElementById("divProvincia").style.display="none";
+  }
+}
+function handleSignUp() {
+
+  $("html").css("cursor", "progress");
+
+
+  var correo1 = document.getElementById("email").value.toLowerCase();
+  var correo2 = document.getElementById("correo2").value.toLowerCase();
+
+  if (correo2 != correo1) {
+    $("html").css("cursor", "default");
+    alert("Los correos no coinciden, evite usar copiar y pegar");
+    return;
+  }
+  else{
+    if (/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(correo1)){
+     
+      } else {
+        $("html").css("cursor", "default");
+      alert("La dirección de email es incorrecta.");
+      return;
+      }
+  }
+
+  var contra1= document.getElementById("password").value;
+  var contra2=document.getElementById("confirmPassword").value;
+  if(contra1!==contra2){
+    $("html").css("cursor", "default");
+    alert("Las contraseñas no coinciden");
+    return;
+  }
+  var usuario = document.getElementById("usuario").value;
+  var db = firebase.firestore();
+  var docRef = db.collection("usuarios").where("nombreUsuario","==",usuario);
+  var existe=false;
+  docRef.get().then(function (querySnapshot){
+      querySnapshot.forEach(function(doc){
+        existe=true;
+      });
+  }).then(function(){
+
+  if(existe==true){
+    $("html").css("cursor", "default");
+    alert("Ya existe ese nombre de usuario");
+    return;
+  }
+  var nombre = document.getElementById("nombre").value;
+  var apellidos = document.getElementById("apellidos").value;
+
+  var perfil = document.getElementById("perfil").value;
+  var altura = document.getElementById("altura").value;
+  var edad = document.getElementById("edad").value;
+  var pais = document.getElementById("pais").value;
+  var provincia = document.getElementById("provincia").value;
+  var peso = document.getElementById("peso").value;
+  var metrosCuadrados = (altura*altura)/10000;
+  var imc = metrosCuadrados/peso;
+
+
+  if(nombre.length<=1){
+    $("html").css("cursor", "default");
+    alert("Debes introducir un nombre válido");
+    return;
+  }
+  if(apellidos.length<=1){
+    $("html").css("cursor", "default");
+    alert("Debes introducir un nombre válido");
+    return;
+  }
+
+  if(peso.includes(",")){
+    $("html").css("cursor", "default");
+    alert("El separador decimal es '.'");
+    return;
+  }
+  if(edad<18){
+    $("html").css("cursor", "default");
+    alert("Debes ser mayor de edad");
+    return;
+  }
+  if(edad>130){
+    $("html").css("cursor", "default");
+    alert("Debes introducir una edad válida");
+    return;
+  }
+  if(altura<50||altura>250){
+    $("html").css("cursor", "default");
+    alert("Debes introducir una altura válida");
+    return;
+  }
+  
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(correo1, contra1)
+    .then(
+      function (user) {
+        //location.href='./continuarregistro.html';
+        var user = firebase.auth().currentUser;
+        //logUser(user);
+
+        var uid2 = user.uid;
+        var db = firebase.firestore();
+    
+        db.collection("usuarios")
+          .doc(user.email)
+          .set({
+            correo: correo1,
+            identificador: uid2,
+            nombreUsuario: usuario,
+            nombre,
+            apellidos,
+            perfil,
+            altura,
+            edad,
+            pais,
+            provincia,
+            peso,
+            imc
+          })
+          .then(function () {
+
+            $("html").css("cursor", "default");
+            alert("Usuario registrado correctamente")
+        
+          
+          })
+          .catch(function (error) {
+            console.error("Error writing document: ", error);
+          });
+
+        //location.href='./continuarregistro.html';
+      },
+      function (error) {
+        // Handle Errors here.
+        var errorboton2 = "1";
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == "auth/weak-password") {
+          alert("The password is too weak.");
+
+          return
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+      }
+    );
+  }); 
+}
