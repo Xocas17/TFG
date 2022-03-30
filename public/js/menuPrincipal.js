6e81aba64dd4c51d5ddf6f2ee4888ed326550496
Xocas17 <xocas89@gmail.com>
2022-03-30 19:51:24 +0200

    avances

--- public/js/menuPrincipal.js
+++ public/js/menuPrincipal.js
@@ -21,9 +21,10 @@ document.addEventListener('DOMContentLoaded', function() {
         var nombre = myData.nombre;
         var apellidos = myData.apellidos;
         var email = user.email;
-
+        var imgUrl = "https://firebasestorage.googleapis.com/v0/b/tfginfo-be99f.appspot.com/o/fotosUsuario%2F" + encodeURIComponent(email) + ".jpeg?alt=media";
         document.getElementById("nombreNav").innerText=nombre +" " +apellidos;
         document.getElementById("emailNav").innerText=email;
+        document.getElementById("imgPerfil").src=imgUrl;
       }
 
     })
