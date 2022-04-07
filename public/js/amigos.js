07b40cad945e4530bb64e32a91951cdaa1878955
Xocas17 <xocas89@gmail.com>
2022-04-07 13:43:46 +0200

    amigosCompletado

--- public/js/amigos.js
+++ public/js/amigos.js
@@ -1,5 +1,6 @@
 var email;
 var usuario;
+
 function añadirAmigo(){
 $("html").css("cursor", "progress");
 var amigoAñadir = document.getElementById("usuarioAñadir").value;
@@ -31,13 +32,71 @@ docRef.get().then(function (querySnapshot){
 
 }
 function borrarAmigo(amigo){
+    $("html").css("cursor", "progress");
+    var db = firebase.firestore();
+    db.collection("usuarios").doc(email).update({
+        amigos: firebase.firestore.FieldValue.arrayRemove(amigo),
+    }).then(function () {
+        var db = firebase.firestore();
+        var docRef = db.collection("usuarios").where("nombreUsuario", "==", amigo);
+        var emailAmigoAñadir;
+        docRef.get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                emailAmigoAñadir = doc.id;
+            })
+        }).then(function () {
+            db.collection("usuarios").doc(emailAmigoAñadir).update({
+                amigos: firebase.firestore.FieldValue.arrayRemove(usuario)
+            }).then(function () {
+                $("html").css("cursor", "default");
+                alert("Amigo eliminado correctamente");
+                location.reload();
+            })
 
-}
-function aceptarSolicitud(amigo){
 
+
+        })
+    })
 }
+function aceptarSolicitud(amigo) {
+    $("html").css("cursor", "progress");
+    var db = firebase.firestore();
+    db.collection("usuarios").doc(email).update({
+        solicitudesAmistad: firebase.firestore.FieldValue.arrayRemove(amigo),
+        amigos: firebase.firestore.FieldValue.arrayUnion(amigo)
+    }).then(function () {
+        var db = firebase.firestore();
+        var docRef = db.collection("usuarios").where("nombreUsuario", "==", amigo);
+        var emailAmigoAñadir;
+        docRef.get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                emailAmigoAñadir = doc.id;
+            })
+        }).then(function () {
+            db.collection("usuarios").doc(emailAmigoAñadir).update({
+                amigos: firebase.firestore.FieldValue.arrayUnion(usuario)
+            }).then(function () {
+                $("html").css("cursor", "default");
+                alert("Solicitud de amistad añadida correctamente");
+                location.reload();
+            })
+
+
+
+        })
+    })
+}
+
 function rechazarSolicitud(amigo){
-
+$("html").css("cursor", "progress");
+var db = firebase.firestore();
+db.collection("usuarios").doc(email).update({
+    solicitudesAmistad: firebase.firestore.FieldValue.arrayRemove(amigo)
+}).then(function(){
+    $("html").css("cursor", "default");
+    alert("Solicitud de amistad rechazada correctamente");
+    location.reload();
+})
 }
 
 function modalOpen(m){
@@ -67,12 +126,12 @@ firebase.auth().onAuthStateChanged(function (user) {
             
             solicitudesAmistad.forEach(function(a){
                 var imgUrl = "https://firebasestorage.googleapis.com/v0/b/tfginfo-be99f.appspot.com/o/fotosUsuario%2F" + encodeURIComponent(a) + ".jpeg?alt=media";
-                var solicitud='<li class="collection-item avatar"><a><img id="imgPerfil'+a+'" class="circle" src="'+imgUrl+'" onerror="this.onerror=null;this.src=\'../src/img/perfilDefault.png\';"></a><center><br><span>'+a+'</span></center><a class="secondary-content"><i class="material-icons">check</i></a><a style="margin-top:2em;" class="secondary-content"><i class="material-icons">close</i></a></li>';
+                var solicitud='<li class="collection-item avatar"><a><img id="imgPerfil'+a+'" class="circle" src="'+imgUrl+'" onerror="this.onerror=null;this.src=\'../src/img/perfilDefault.png\';"></a><center><br><span>'+a+'</span></center><a onclick="aceptarSolicitud(\''+a+'\')" style="cursor:pointer" class="secondary-content"><i class="material-icons">check</i></a><a onclick="rechazarSolicitud(\''+a+'\')" style="cursor:pointer;margin-top:2em;" class="secondary-content"><i class="material-icons">close</i></a></li>';
                 $('#solicitudesAmistad').append(solicitud)
             })
             amigos.forEach(function(a){
                 var imgUrl = "https://firebasestorage.googleapis.com/v0/b/tfginfo-be99f.appspot.com/o/fotosUsuario%2F" + encodeURIComponent(a) + ".jpeg?alt=media";
-                var amigo='<li class="collection-item avatar"><a><img id="imgPerfil'+a+'" class="circle" src="'+imgUrl+'" onerror="this.onerror=null;this.src=\'../src/img/perfilDefault.png\';"></a><center><br><span>'+a+'</span></center><a class="secondary-content"><i class="material-icons">delete</i></a></li>';
+                var amigo='<li class="collection-item avatar"><a><img id="imgPerfil'+a+'" class="circle" src="'+imgUrl+'" onerror="this.onerror=null;this.src=\'../src/img/perfilDefault.png\';"></a><center><br><span>'+a+'</span></center><a onclick="borrarAmigo(\''+a+'\')" style="cursor:pointer" class="secondary-content"><i class="material-icons">delete</i></a></li>';
                 $('#listaAmigos').append(amigo)
             })
 
